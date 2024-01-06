4ae3538b7be2d503b4a9fec645c9521a397e0bf0
soo1e <gameswuon@gmail.com>
2024-01-06 19:30:29 +0900

    0106

---
+++ src/HelloJava.java
@@ -0,0 +1,6 @@
+public class HelloJava {
+    // main 메서드라고 한다. 여기서 프로그램을 시작한다.
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}
---
+++ src/variable/Var3.java
@@ -0,0 +1,14 @@
+package variable;
+
+public class Var3 {
+    public static void main(String[] args) {
+        int a; // 변수 선언
+        a = 10; // 변수 초기화 -> 변수에 초기 값을 넣음.
+
+        System.out.println(a);
+
+        a = 50; // 변수 값 변경
+
+        System.out.println(a);
+    }
+}
