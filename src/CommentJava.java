a783e6e53d9741b4884672eb014f87f77f6e7dd4
soo1e <gameswuon@gmail.com>
2024-01-07 15:26:40 +0900

    Sec 2

---
+++ src/CommentJava.java
@@ -0,0 +1,11 @@
+public class CommentJava {
+    public static void main(String[] args) {
+        System.out.println("Hello Java1"); // Hello Java1을 출력한다.
+        System.out.println("Hello Java2"); // Hello Java2을 출력한다.
+//        System.out.println("Hello Java3"); // Hello Java3을 출력한다.
+
+        /*
+        별 주석
+         */
+    }
+}
