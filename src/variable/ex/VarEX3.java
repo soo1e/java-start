519513a760fbe2150a06875000c4bb188f94ca57
soo1e <gameswuon@gmail.com>
2024-01-07 15:26:11 +0900

    Sec 2

---
+++ src/variable/ex/VarEX3.java
@@ -0,0 +1,11 @@
+package variable.ex;
+
+public class VarEX3 {
+    public static void main(String[] args) {
+        long longVar = 10000000000L;
+        boolean bool = true;
+
+        System.out.println(longVar);
+        System.out.println(bool);
+    }
+}
