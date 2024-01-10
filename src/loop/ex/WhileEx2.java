9af9eb857b1bda33d7f5779dfd5e1ae9ae18005a
soo1e <gameswuon@gmail.com>
2024-01-10 11:51:59 +0900

    Sec 5

---
+++ src/loop/ex/WhileEx2.java
@@ -0,0 +1,11 @@
+package loop.ex;
+
+public class WhileEx2 {
+    public static void main(String[] args) {
+        int num = 2;
+        while (num <= 20) {
+            System.out.println(num);
+            num = num + 2;
+        }
+    }
+}
